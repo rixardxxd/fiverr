70ca9673cd1d03df07a50c29cca82aae84b45f64
stevenhan2012 <binghan87@gmail.com>
2014-08-29 22:44:17 -0700

    add models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Gig(models.Model):
+    class Meta:
+        verbose_name = ""
+
+    gig_id = models.AutoField(primary_key=True)
+    seller_id = models.ForeignKey('User')
+    rating_avg = models.FloatField(default=0.0)
+    rating_count = models.IntegerField(default=0)
+
+
+    def __unicode__(self):
+        return u'gig_id: {} ____ seller_id: {} ____ rating_avg: {} ____ rating_count: {}'.format(self.name, self.price, self.size, self.city, self.img)
+
