3265290d76286469570288c4c62c7db6666053ab
stevenhan2012 <binghan87@gmail.com>
2014-09-02 22:35:09 -0700

    change item to gig

--- website/views.py
+++ website/views.py
@@ -5,31 +5,31 @@ from django.template import RequestContext
 
 def main_view(request):
     #mock the data right now
-    item_list = list()
-    item1 = dict()
-    item1['img'] = '/static/img/img1.jpeg'
-    item1['name'] = u'第一项服务'
+    gigs = list()
+    gig1 = dict()
+    gig1['img'] = '/static/img/img1.jpeg'
+    gig1['title'] = u'第一项服务'
 
-    item2 = dict()
-    item2['img'] = '/static/img/img2.jpeg'
-    item2['name'] = u'第二项服务'
+    gig2 = dict()
+    gig2['img'] = '/static/img/img2.jpeg'
+    gig2['title'] = u'第二项服务'
 
-    item3 = dict()
-    item3['img'] = '/static/img/img3.jpeg'
-    item3['name'] = u'第三项服务'
+    gig3 = dict()
+    gig3['img'] = '/static/img/img3.jpeg'
+    gig3['title'] = u'第三项服务'
 
-    item4 = dict()
-    item4['img'] = '/static/img/img4.jpeg'
-    item4['name'] = u'第四项服务'
+    gig4 = dict()
+    gig4['img'] = '/static/img/img4.jpeg'
+    gig4['title'] = u'第四项服务'
 
-    item5 = dict()
-    item5['img'] = '/static/img/img5.jpeg'
-    item5['name'] = u'第五项服务'
+    gig5 = dict()
+    gig5['img'] = '/static/img/img5.jpeg'
+    gig5['title'] = u'第五项服务'
 
-    item_list.append(item1)
-    item_list.append(item2)
-    item_list.append(item3)
-    item_list.append(item4)
-    item_list.append(item5)
-    context_dict = {'list_items':item_list}
+    gigs.append(gig1)
+    gigs.append(gig2)
+    gigs.append(gig3)
+    gigs.append(gig4)
+    gigs.append(gig5)
+    context_dict = {'gigs':gigs}
     return render_to_response('website/main.html',RequestContext(request, context_dict))
