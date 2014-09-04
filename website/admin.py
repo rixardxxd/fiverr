95f69153bbdc5578f849d8a42c57900bf928c5ba
xxd <xxd@yahoo-inc.com>
2014-09-03 21:20:36 -0700

    detail view

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Gig, Category, SubCategory, Tag
 
-# Register your models here.
+admin.site.register(Gig)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Tag)
--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, include, url
 import views,registration_views
 
 urlpatterns = patterns('',
-    url(r'^$', views.main_view, name="main"),
+    url(r'^$', views.main_view, name="main_view"),
     url(r'^login/$', registration_views.login_view, name="login"),
     url(r'^logout/$', registration_views.logout_view, name="logout"),
-    url(r'^register/$', registration_views.register_view, name="register")
+    url(r'^register/$', registration_views.register_view, name="register"),
+    url(r'^detail-view/(?P<gigid>[-\d]+)/$',views.detail_view, name="detail_view")
 )
 
